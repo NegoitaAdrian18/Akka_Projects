1e39c060e4214dc8a63e382d9331ff1dd51a5468
NegoitaAdrian18 <104589799+NegoitaAdrian18@users.noreply.github.com>
2023-02-07 12:41:15 +0200

    Delete main.py

--- passwordGenerator/main.py
+++
@@ -1,8 +0,0 @@
-import random
-
-password = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*():?|/"
-lenght_pass = int(input("Enter the length of the password: "))
-result = "".join(random.sample(password, lenght_pass))
-
-print(f"The final password is: {result}")
-
